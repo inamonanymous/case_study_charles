21603722b392950b89dc273c545a798a65fc20c1
Stephen Aguilar <stephenonline25@gmail.com>
2023-11-14 09:57:48 +0800

    project almost finished

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, url_for, session, request, jsonify
-from model import db, TradeData, Products, Location, SalesStatistics
+from model import db, TradeData, Products, Location, SalesStatistics, update_sales_statistics
+from sqlalchemy import func, distinct
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -7,22 +8,116 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/farmerdata_char
 db.init_app(app)
 migrate = Migrate(app, db)
 
+@app.route('/add-trade', methods=['POST', 'GET'])
+def add_trade():
+    if request.method=="POST":
+        data = request.get_json()
+        trade_entry = TradeData(
+            location_id = data['location_id'],
+            product_id = data['trade_product'],
+            trade_year = data['trade_year'],
+            trade_quantity = data['trade_quantity']
+        )
+        db.session.add(trade_entry)
+        db.session.commit()
+        return jsonify({"message": "success"}), 201
+    return jsonify({"message": "error"}), 401
+
+@app.route('/get-products-and-locations')
+def get_product_and_location():
+    locations = Location.query.all()
+    products = Products.query.all()
+    locations_data = [
+        {
+            'location_id': loc.location_id, 
+            'location_name': loc.location_name
+         }
+        for loc in locations
+    ]
+
+    products_data = [
+        {
+            'product_id': prod.product_id,
+            'product_name': prod.product_name,
+            'product_description': prod.product_description,
+            'product_unit': prod.product_unit
+        } for prod in products
+    ]
+
+    data = {
+        'locations_data': locations_data,
+        'products_data': products_data
+    }
+
+    return jsonify(data)
+
+@app.route('/api/reports_data')
+def reports():
+    locations = Location.query.all()
+    products = Products.query.all()
+    trade_data = TradeData.query.\
+    with_entities(TradeData.trade_id,
+                  TradeData.trade_year,
+                  TradeData.trade_quantity,
+                  Products.product_name,
+                  Products.product_description,
+                  Location.location_name, 
+                  ).\
+    join(Products, TradeData.product_id == Products.product_id).\
+    join(Location, TradeData.location_id==Location.location_id).all()
+    print(trade_data)
+    sales_statistics = SalesStatistics.query.all()
+
+    locations_list = [{'location_id': i.location_id, 
+                       'location_name': i.location_name} for i in locations]
+    
+    products_list = [{'product_id': i.product_id, 
+                      'product_name': i.product_name, 
+                      'product_description': i.product_description, 
+                      'product_unit': i.product_unit} for i in products]
+    
+    trades_list = [{'trade_id': i.trade_id, 
+                    'location_name': i.location_name,
+                    'product_name': i.product_name,
+                    'trade_year': i.trade_year, 
+                    'trade_quantity': i.trade_quantity} for i in trade_data]
+    
+    stat_list = [{'stat_id': i.stat_id, 
+                       'stat_year': i.stat_year,
+                       'stat_series_name': i.stat_series_name,
+                       'stat_value': i.stat_value} for i in sales_statistics]
+    
+
+    reports = {
+        'locations': locations_list,
+        'products': products_list,
+        'trade_data': trades_list,
+        'sales_statistics': stat_list
+    }
+
+    return jsonify(reports)
+
 #radar data
 @app.route('/api/radar_chart_data')
 def radar_chart_data():
-    # Assume you are tracking data for years 2018 to 2023
-    years = ['2018', '2019', '2020', '2021', '2022', '2023']
-    series_data = {}
+    # Here we assume you are comparing sales volume across different products
+    sales_volume_by_product = db.session.query(
+        Products.product_name,
+        db.func.sum(SalesStatistics.sales_volume).label('total_sales_volume')
+    ).join(SalesStatistics, SalesStatistics.product_id == Products.product_id)\
+     .group_by(Products.product_name)\
+     .all()
 
-    query_results = SalesStatistics.query.all()
-    for result in query_results:
-        series_name = result.stat_series_name
-        if series_name not in series_data:
-            series_data[series_name] = [0] * len(years)
-        year_index = years.index(result.stat_year)
-        series_data[series_name][year_index] = result.stat_value
+    # Format for ApexCharts radar chart
+    response = {
+        'series': [{
+            'name': 'Sales Volume',
+            'data': [volume for _, volume in sales_volume_by_product],
+        }],
+        'categories': [product for product, _ in sales_volume_by_product]
+    }
 
-    return jsonify(series_data)
+    return jsonify(response)
 
 #trade data 
 @app.route('/api/area_chart_data')
@@ -58,15 +153,7 @@ def get_bar_chart_data():
         if i not in bar_chart_data:
             bar_chart_data[i] = []
         bar_chart_data[i].append(x)
-
-        
-    """bar_chart_data = {
-        "Rice": [10, 20, 15, 25, 30, 18, 12, 24, 16],
-        "Sugar": [5, 15, 10, 20, 25, 12, 8, 22, 14],
-        "Oil": [8, 18, 13, 23, 28, 15, 10, 20, 12],
-        "Vegetable": [12, 22, 17, 37, 32, 20, 14, 26, 18]
-    }"""
-
+       
     # Include the x-axis categories in the API response
     xaxis_categories = [i.location_name for i in Location.query.all()]
     response_data = {
@@ -76,6 +163,23 @@ def get_bar_chart_data():
         "data": bar_chart_data
     }
     return jsonify(response_data)
+
+@app.route('/dashboard-items')
+def dashboard_items():
+    products_count = Products.query.with_entities(func.count(distinct(Products.product_id))).scalar()
+    sales_count = SalesStatistics.query.with_entities(func.count(distinct(SalesStatistics.stat_id))).scalar()
+    trades_count = TradeData.query.with_entities(func.count(distinct(TradeData.trade_id))).scalar()
+    location_count = Location.query.with_entities(func.count(distinct(Location.location_id))).scalar()
+    
+    data = {
+        'products_count': products_count,
+        'sales_count': sales_count,
+        'trades_count': trades_count,
+        'location_count': location_count
+    }
+    
+    return jsonify(data)
+
 
 @app.route('/')
 def index():
--- model.py
+++ model.py
@@ -16,14 +16,57 @@ class Products(db.Model):
 class TradeData(db.Model):
     __tablename__ = 'trade_data'
     trade_id = db.Column(db.Integer, primary_key=True)
-    location_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
+    
+    # Define foreign keys for location_id and product_id
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'))
+    
     trade_year = db.Column(db.String(4))
     trade_quantity = db.Column(db.Integer)
 
+    # Define relationships to Location and Products
+    location = db.relationship('Location', backref='trade_data', foreign_keys=[location_id])
+    product = db.relationship('Products', backref='trade_data', foreign_keys=[product_id])
+   
 class SalesStatistics(db.Model):
     __tablename__ = 'sales_statistics'
     stat_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
     stat_year = db.Column(db.String(4))
-    stat_series_name = db.Column(db.String(255))
-    stat_value = db.Column(db.Integer)
+    sales_volume = db.Column(db.Integer)
+
+    product = db.relationship('Products', foreign_keys=[product_id])
+    location = db.relationship('Location', foreign_keys=[location_id])
+
+
+
+def update_sales_statistics():
+    # Query TradeData
+    trade_data = TradeData.query.all()
+
+    # Process each trade entry
+    for trade in trade_data:
+        # Logic to aggregate or calculate statistics
+        # Example: summing trade quantities for a product
+        existing_stat = SalesStatistics.query.filter_by(
+            product_id=trade.product_id, 
+            location_id=trade.location_id,
+            stat_year=trade.trade_year
+        ).first()
+
+        if existing_stat:
+            # Update existing statistic
+            existing_stat.sales_volume += trade.trade_quantity
+        else:
+            # Create a new statistic entry
+            new_stat = SalesStatistics(
+                product_id=trade.product_id,
+                location_id=trade.location_id,
+                stat_year=trade.trade_year,
+                sales_volume=trade.trade_quantity
+            )
+            db.session.add(new_stat)
+    
+    # Commit changes to the database
+    db.session.commit()
